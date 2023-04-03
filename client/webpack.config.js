f178f58621dd7abf6549f9c516a5be928da441be
Adrien Saint-Vanne <adriensv@gmail.com>
2023-04-04 01:21:20 +0200

    Update webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -45,8 +45,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./public/index.html",
       favicon: "./public/favicon.ico",
-    }),
-    new Dotenv()
+    })
   ],
   stats: "minimal",
 };
